f10b5a18bfa8705b8cf88a2d1067833e1e9fa06d
Joe Cooper <josephcooper3@gmail.com>
2019-08-05 22:13:49 +0100

    refactored library tests

--- specs/library_spec.rb
+++ specs/library_spec.rb
@@ -31,11 +31,13 @@ class TestLibrary < MiniTest::Test
   end
 
   def test_get_book
-    assert_equal({:title=>"learn_to_program", :rental_details=>{:student_name=>"Joe", :date=>"01/05/19"}}, @library.get_book(0))
+    result = @library.get_book(0)
+    assert_equal({:title=>"learn_to_program", :rental_details=>{:student_name=>"Joe", :date=>"01/05/19"}}, result)
   end
 
   def test_book_information_from_title__book_found
-    assert_equal({:title=>"learn_to_program", :rental_details=>{:student_name=>"Joe", :date=>"01/05/19"}}, @library.book_information_from_title("learn_to_program"))
+    result = @library.book_information_from_title("learn_to_program")
+    assert_equal({:title=>"learn_to_program", :rental_details=>{:student_name=>"Joe", :date=>"01/05/19"}}, result)
   end
 
   def test_book_information_from_title__book_not_found
@@ -43,7 +45,8 @@ class TestLibrary < MiniTest::Test
   end
 
   def test_rental_details_from_title__book_found
-    assert_equal({:student_name=>"Joe", :date=>"01/05/19"}, @library.rental_details_from_title("learn_to_program"))
+    result = @library.rental_details_from_title("learn_to_program")
+    assert_equal({:student_name=>"Joe", :date=>"01/05/19"}, result)
   end
 
   def test_rental_details_from_title__book_not_found
@@ -51,13 +54,14 @@ class TestLibrary < MiniTest::Test
   end
 
   def test_add_book_to_library
-    assert_equal({:title=>"Schott's Miscellany", :rental_details=>{:student_name=>"", :date=>""}}, @library.add_book_to_library("Schott's Miscellany").last)
+    result = @library.add_book_to_library("Schott's Miscellany").last
+    assert_equal({:title=>"Schott's Miscellany", :rental_details=>{:student_name=>"", :date=>""}}, result)
   end
 
   def test_borrow_book
     @library.borrow_book("harry_potter", "Joe", "31/08/2019")
-    assert_equal("Joe", @library.book_information_from_title("harry_potter")[:rental_details][:student_name])
-    assert_equal("31/08/2019", @library.book_information_from_title("harry_potter")[:rental_details][:date])
+    assert_equal("Joe", @library.get_book(2)[:rental_details][:student_name])
+    assert_equal("31/08/2019", @library.get_book(2)[:rental_details][:date])
   end
 
 end
