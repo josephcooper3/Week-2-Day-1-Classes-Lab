8418460b50b29a549630821e59e77103495f00a0
Joe Cooper <josephcooper3@gmail.com>
2019-08-05 16:52:06 +0100

    Initialized Team class

---
+++ specs/team_spec.rb
@@ -0,0 +1,7 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../team')
+
+class TestTeam < MiniTest::Test
+
+end
---
+++ team.rb
@@ -0,0 +1,9 @@
+class Team
+
+  def initialize(input_name, input_players, input_coach)
+    @name = input_name
+    @players = input_players
+    @coach = input_coach
+  end
+
+end
